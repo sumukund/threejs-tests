eba3e7c57cc0fcd1719b09e64894216aaa6f1ef5
sumukund <mukund.su@husky.neu.edu>
2025-10-22 11:03:31 -0500

    getting the cube up

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
@@ -6,3 +7,7 @@ app = Flask(__name__)
 @app.route('/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
+
+@app.route('/main.js')
+def main_js():
+    return send_from_directory(os.path.join(app.root_path, 'templates'), 'main.js')
